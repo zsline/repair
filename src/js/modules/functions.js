6f8975799338c0b054490d4840d71244163c7935
zsline <sergei.dereka@gmail.com>
2023-09-05 15:35:26 +0300

    repair-1

---
+++ src/js/modules/functions.js
@@ -0,0 +1,30 @@
+/* Проверка поддержки webp, добавление класса для html */
+export function isWebp() {
+    //проверка поддержки webp
+    function testWebP(callback) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    //добавление класса _webp или _no-webp для HTML
+    testWebP(function (support) {
+        let className = support === true ? 'webp' : 'no-webp';
+        document.documentElement.classList.add(className);
+    });
+}
+
+//открытие/закрытие меню
+
+const nav_btn = document.querySelector('.header__icon');
+const nav_menu = document.querySelector('.header__nav');
+nav_btn.addEventListener('click', function() {
+    nav_menu.classList.toggle('active');
+});
+
+if(nav_menu.classList.contains('active')){
+    document.addEventListener('click', function(){
+        nav_menu.classList.remove('active');
+    });
+};
